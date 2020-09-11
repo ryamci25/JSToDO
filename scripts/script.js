253aceaff960820a3eb900022315ae7080a4420a
ryamci25 <ryamci25@hamiltonchristian.school.nz>
2020-09-11 14:50:29 +1200

    added bootstrap files

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,26 @@
 function getData() {
 
     fetch('scripts/data.json')
-    .then(resp => resp.json)
-    .then(data => console.log(data))
+    .then(resp => resp.json())
+    .then(data => {
+
+    let htmlStuff = data.map(d => {
+     return `
+     <tr>
+     <td>
+     <input type="checkbox" vaule="${d.completed}" ${d.completed ? "checked" : null } />
+     </td>
+     <td>${d.title}</td>
+     <td>${d.completed}</td>
+     </tr>`
+    }).join("")
+
+    //console.log(htmlStuff)
+    document.querySelector('tbody').innerHTML = htmlStuff
+
+
+
+    })
 } 
 
 getData()
